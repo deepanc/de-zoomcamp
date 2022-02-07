606431af46a88260da01991aeedb72fba6621a3a
Deepa <deepa@MacBook-Air.local>
2022-02-07 06:49:38 +0530

    code for week2 assignments

---
+++ week2/airflow/dags/data_ingestion_nyc_fhv_dag.py
@@ -0,0 +1,114 @@
+import os
+import logging
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+from google.cloud import storage
+import pyarrow.csv as pv
+import pyarrow.parquet as pq
+import pyarrow as pa
+import pandas as pd
+import glob
+
+PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
+BUCKET = os.environ.get("GCP_GCS_BUCKET")
+AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+URL_PREFIX = 'https://nyc-tlc.s3.amazonaws.com/trip+data' 
+URL_TEMPLATE = URL_PREFIX + '/fhv_tripdata_{{ execution_date.strftime(\'%Y-%m\') }}.csv'
+OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME + '/fhv_output_{{ execution_date.strftime(\'%Y-%m\') }}.csv'
+PARQUET_FILE_TEMPLATE = 'fhv_output_{{ execution_date.strftime(\'%Y-%m\') }}.parquet'
+PARQUET_FILE_LOCATION = AIRFLOW_HOME + '/fhv_output_{{ execution_date.strftime(\'%Y-%m\') }}.parquet'
+
+
+def format_to_parquet(src_file):
+    if not src_file.endswith('.csv'):
+        logging.error("Can only accept source files in CSV format, for the moment")
+        return
+    table = pv.read_csv(src_file)
+    pq.write_table(table, src_file.replace('.csv', '.parquet'))
+    
+    # Used the below code to upload only FHV Jan 2019 dataset as it was huge
+
+    # dst_folder = src_file.replace('.csv', '_parquet')
+    # df_iter = pd.read_csv(src_file, iterator = True, chunksize = 500000)
+
+    # while True:
+    #     try: 
+    #         df = next(df_iter)
+    #         table_pq = pa.Table.from_pandas(df)
+    #         pq.write_to_dataset(table_pq, root_path=dst_folder)
+    #         logging.info(f'saved another {len(df)} lines to parquet file')
+    #     except (StopIteration):
+    #         logging.info('EOF reached')
+    #         break
+
+
+# NOTE: takes 20 mins, at an upload speed of 800kbps. Faster if your internet has a better upload speed
+def upload_to_gcs(bucket, object_name, local_file):
+    """
+    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
+    :param bucket: GCS bucket name
+    :param object_name: target path & file-name
+    :param local_file: source path & file-name
+    :return:
+    """
+    # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
+    # (Ref: https://github.com/googleapis/python-storage/issues/74)
+    storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
+    storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
+    # End of Workaround
+
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+
+    blob = bucket.blob(object_name)
+    blob.upload_from_filename(local_file)
+
+    # Used the below code to upload only FHV Jan 2019 dataset as it was huge
+
+    # files = glob.glob(local_pq_folder + '*.parquet')
+
+    # for file in files:
+    #     print(f'uploading {file} to {blob}')
+    #     blob.upload_from_filename(file)
+
+local_workflow = DAG(
+    "data_ingestion_nyc_fhv_dag",
+    schedule_interval="0 6 2 * *",
+    start_date=datetime(2019, 1, 1),
+    end_date=datetime(2021, 1, 1),
+    catchup=True,
+    max_active_runs=1,
+    tags=['dtc-hw-de']
+)
+
+# NOTE: DAG declaration - using a Context Manager (an implicit way)
+with local_workflow:
+    download_dataset_task = BashOperator(
+        task_id="download_dataset_task",
+        bash_command=f"curl -sSL {URL_TEMPLATE} > {OUTPUT_FILE_TEMPLATE}"
+    )
+
+    format_to_parquet_task = PythonOperator(
+        task_id="format_to_parquet_task",
+        python_callable=format_to_parquet,
+        op_kwargs={
+            "src_file": f"{OUTPUT_FILE_TEMPLATE}",
+        },
+    )
+
+    local_to_gcs_task = PythonOperator(
+        task_id="local_to_gcs_task",
+        python_callable=upload_to_gcs,
+        op_kwargs={
+            "bucket": BUCKET,
+            "object_name": f"raw/{PARQUET_FILE_TEMPLATE}",
+            "local_file": f"{PARQUET_FILE_LOCATION}",
+            # "local_pq_folder": OUTPUT_FILE_TEMPLATE.replace('.csv', '_parquet/'),
+        },
+    )
+
+    download_dataset_task >> format_to_parquet_task >> local_to_gcs_task
